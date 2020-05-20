25c96dccb825675c8441f51e88f5e1c9c88c7003
EduardoYetmo <eduardo.yetmo@gmail.com>
2020-05-20 00:13:03 -0500

    Se agrego el archivo SalarioDH

---
+++ Practica1/SalarioDH.java
@@ -0,0 +1,17 @@
+public class SalarioDH{
+    public static void main (final String[] args){
+
+        int Sueldoxhr=30;
+        int Hrxdia=8;
+        int dias=15;
+        int Salario;
+
+        Salario = Sueldoxhr * Hrxdia * dias;
+
+        System.out.println("El salario que gana un trabajador por quincena es:");
+        System.out.println("$"+Salario);
+
+
+}
+
+}
