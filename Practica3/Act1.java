2cd62a0ac8d71825f7f8f372c7dc2682fc147ab2
EduardoYetmo <eduardo.yetmo@gmail.com>
2020-06-14 14:57:20 -0500

    Actualización

---
+++ Practica3/Act1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner; 
+
+public class Act1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String c = sc.next();
+        double suma = 0;
+        int cont = 0;
+
+        while( !c.equals("FIN") ){
+        switch(c){
+            case "NA": suma += 5.0;
+                        break;
+            case "S": suma += 6.0;
+                        break;
+            case "B": suma += 8.0;
+                        break;
+            case "MB": suma += 10.0;
+                        break;
+            
+            case default: cont++;
+
+            System.out.println(c);
+            System.out.println("Escribe tu calificación; ");
+            c = sc.next();
+
+
+
+    }
+}
+    if(cont == 0){
+        System.out.println("No escribiste nada");
+    }else{
+
+    System.out.println("Tu promedio es: " + suma/cont);
+    }
+    
+}
+}
